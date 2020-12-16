1f7aa0af7f1b3924e14d7aa006cd1eecd579b9b0
GrandPa <75545155+grandparon@users.noreply.github.com>
2020-12-15 18:08:49 -0800

    tried lights in background

--- main.ts
+++ main.ts
@@ -1,24 +1,34 @@
-let x = 1
 /**
  * Cutebot with color changing LEDs
  */
+cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0x00ffff)
+cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0x00ffff)
+basic.pause(200)
+cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff8000)
+cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff8000)
+basic.pause(200)
+cuteBot.closeheadlights()
 basic.forever(function () {
     basic.pause(50)
     cuteBot.moveTime(cuteBot.Direction.forward, 20, 0.2)
     cuteBot.motors(25, 10)
     basic.pause(4000)
-    cuteBot.moveTime(cuteBot.Direction.forward, 20, 0.19)
     cuteBot.moveTime(cuteBot.Direction.forward, 20, 0.2)
     cuteBot.motors(10, 25)
     basic.pause(4000)
     cuteBot.moveTime(cuteBot.Direction.forward, 20, 0.2)
-    if (x) {
-        x = 0
-        cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff0000)
-        cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0xff0000)
-    } else {
-        cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0x007fff)
-        cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0x007fff)
-        x = 1
+    cuteBot.moveTime(cuteBot.Direction.forward, 20, 0.19)
+})
+control.inBackground(function () {
+    basic.pause(2000)
+    while (true) {
+        basic.pause(2000)
+        if (input.lightLevel() <= 0) {
+            cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xffff00)
+            cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0xff8000)
+        } else {
+            cuteBot.closeheadlights()
+        }
+        basic.pause(100)
     }
 })
